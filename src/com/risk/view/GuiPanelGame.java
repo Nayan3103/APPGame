339044d052cd7329cbe66a756a3f36c66dfa915a
sukhmeet13 <40044862+sukhmeet13@users.noreply.github.com>
2018-11-08 16:04:55 -0800

    Update GuiPanelGame.java

--- src/com/risk/view/GuiPanelGame.java
+++ src/com/risk/view/GuiPanelGame.java
@@ -11,13 +11,14 @@ import javax.swing.JPanel;
  * This class is used for the lower window of the main frame 
  * 
  */
-public class GuiPanelGame extends JPanel {
+public class GuiPanelGame extends JPanel{
 
 	public GuiPanelGame(int width, int height) {
 		this.setPreferredSize(new Dimension(width, height));
 		this.setBackground(Color.CYAN);
 		this.setLayout(new FlowLayout());
 
+	
 	}
 
 }
