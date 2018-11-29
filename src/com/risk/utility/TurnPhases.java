3d236d7c2d91cbc05889aa484f4361c062231bec
sukhmeet13 <40044862+sukhmeet13@users.noreply.github.com>
2018-11-29 15:41:23 -0800

    Update TurnPhases.java

--- src/com/risk/utility/TurnPhases.java
+++ src/com/risk/utility/TurnPhases.java
@@ -1,5 +1,5 @@
 package com.risk.utility;
-// this is showing all phases of game .
+// this is showing all phases of game like reiforcement 
 public enum TurnPhases {
 	Startup, Reinforcement, Fortification, Attack, PreGame, GameOver
 }
