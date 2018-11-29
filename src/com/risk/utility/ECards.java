db69866a026cdb9afe4ba83018358347a582c85c
sukhmeet13 <40044862+sukhmeet13@users.noreply.github.com>
2018-11-29 15:39:50 -0800

    Update ECards.java

--- src/com/risk/utility/ECards.java
+++ src/com/risk/utility/ECards.java
@@ -4,7 +4,7 @@ package com.risk.utility;
  * this is enum for cards and  it has three types named  infantry, cavalry, or artillery
  * 
  * @author Sukhmeet Kaur 
- * @version 1.0.0.0
+ * @version 1.0.0
  */
 public enum ECards {
 	Infantry, Cavalry, Artillery
